eda32f6ff19c4169889fb6200d0de5443632d936
谢彪 <hhxsv5@sina.com>
2020-12-28 19:17:40 +0800

    decode query to fix sign fail

--- src/Auth.php
+++ src/Auth.php
@@ -17,6 +17,21 @@ class Auth implements IAuth
 
     public function signature($requestUri, $body, $timestamp, $method)
     {
+        // Decode to the original text
+        $parts = parse_url($requestUri);
+        if (isset($parts['query'])) {
+            parse_str($parts['query'], $queries);
+            $queryString = '';
+            foreach ($queries as $key => $value) {
+                $queryString .= sprintf('%s=%s&', $key, $value);
+            }
+            $queryString = rtrim($queryString, '&');
+            $requestUri = $parts['path'] . '?' . $queryString;
+            if (isset($parts['fragment'])) {
+                $requestUri .= '#' . $parts['fragment'];
+            }
+        }
+
         if (is_array($body)) {
             $body = empty($body) ? '' : json_encode($body, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
         } else {
